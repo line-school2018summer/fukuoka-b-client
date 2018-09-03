2c594eae5dc57c8f04b557ae155908d71d656d83
Po <colagarychen@gmail.com>
2018-09-03 21:34:22 +0900

    Utility SharedPreference class

---
+++ app/src/main/java/com/line/fukuokabclient/Utility/Prefs.kt
@@ -0,0 +1,11 @@
+package com.line.fukuokabclient.Utility
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class Prefs(val context: Context) {
+    companion object {
+        private const val PREFERENCE_FILE_KEY = "com.line.fukuokabclient.PREFERENCE"
+        fun get(context: Context): SharedPreferences = context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE)
+    }
+}
