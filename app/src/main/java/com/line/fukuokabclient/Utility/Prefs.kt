cd0f2e09cf1b52ea02299b1d247223331b3fdaf0
Po <colagarychen@gmail.com>
2018-09-03 21:41:23 +0900

    refactor Prefs class

--- app/src/main/java/com/line/fukuokabclient/Utility/Prefs.kt
+++ app/src/main/java/com/line/fukuokabclient/Utility/Prefs.kt
@@ -7,5 +7,6 @@ class Prefs(val context: Context) {
     companion object {
         private const val PREFERENCE_FILE_KEY = "com.line.fukuokabclient.PREFERENCE"
         fun get(context: Context): SharedPreferences = context.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE)
+        fun edit(context: Context): SharedPreferences.Editor = get(context).edit()
     }
 }
