83545500a9f8ec4e338dd620cbacf59ae1e495d0
Po <colagarychen@gmail.com>
2018-09-17 21:12:54 +0900

    db setup

---
+++ app/src/main/java/com/line/fukuokabclient/db/ColorData.kt
@@ -0,0 +1,9 @@
+package com.line.fukuokabclient.db
+
+import android.graphics.Color
+
+data class ColorData (
+        var channelId: Long,
+        var userId: Long,
+        var colorCode: Int
+)
--- app/src/main/java/com/line/fukuokabclient/db/DbOpenHelper.kt
+++ app/src/main/java/com/line/fukuokabclient/db/DbOpenHelper.kt
@@ -2,18 +2,31 @@ package com.line.fukuokabclient.db
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
-import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+import org.jetbrains.anko.db.*
 
 class DbOpenHelper(context: Context): ManagedSQLiteOpenHelper(context, "clientDB", null, 1) {
+    private val MSG_COLOR_MAP: String = "msgColorMap"
     companion object {
+        private var instance: DbOpenHelper? = null
 
+        @Synchronized
+        fun getInstance(context: Context): DbOpenHelper {
+            return instance ?: DbOpenHelper(context.applicationContext)
+        }
     }
+
     override fun onCreate(db: SQLiteDatabase?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.run {
+            createTable(MSG_COLOR_MAP, true, columns = *arrayOf(
+                "channelId" to INTEGER,
+                "userId" to INTEGER,
+                "colorCode" to TEXT
+            ))
+        }
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        db?.dropTable(MSG_COLOR_MAP, true)
     }
 
 }
